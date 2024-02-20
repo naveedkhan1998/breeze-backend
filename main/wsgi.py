ab33f574e0e896d9ae9e9032b253bfa44e2e02ed
nkhan364 <naveedkhan13041998@gmail.com>
2024-02-20 17:02:30 -0500

    wsgi

--- main/wsgi.py
+++ main/wsgi.py
@@ -1,16 +1,23 @@
-"""
-WSGI config for main project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
+# This file contains the WSGI configuration required to serve up your
+# web application at http://dtemplarsarsh.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI handler of some
+# description.
+#
+# The below has been auto-generated for your Django project
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# add your project directory to the sys.path
+project_home = '/home/dtemplarsarsh/breeze-backend'
+sys.path.append('/home/dtemplarsarsh/breeze-backend')
+sys.path.append('/home/dtemplarsarsh/breeze-backend/venv/lib/python3.10/site-packages')
 
+# set environment variable to tell django where your settings.py is
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
+
+# serve django via WSGI
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
