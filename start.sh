57611185af5d79407b8b2b4d84333486e8ab0535
nkhan364 <naveedkhan13041998@gmail.com>
2024-07-08 15:55:25 -0400

    update

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-python3 manage.py collectstatic --noinput
-python3 manage.py wait_for_db
-python3 manage.py makemigrations
-python3 manage.py migrate --noinput
-python3 manage.py initadmin
-celery -A main worker --loglevel=INFO --time-limit=300
+
+echo "STARTING GUNICORN SERVER..."
+gunicorn main.wsgi:application -t 1800 --bind :5000 --daemon
+celery -A main worker --loglevel=INFO --time-limit=0
