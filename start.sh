718b8aa73293573affb2a0e9405392a14183450e
nkhan364 <naveedkhan13041998@gmail.com>
2024-07-08 16:38:18 -0400

    ccccc

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 
-# echo "STARTING HTTP SERVER..."
-# python http_server.py &
+# Set the maximum memory limit to 300 MB
+ulimit -v $((300 * 1024))
+
+echo "STARTING HTTP SERVER..."
+python3 http_server.py 
 
 echo "STARTING CELERY WORKER WITH MEMORY LIMIT..."
-python3 start_celery.py
+celery -A main worker --loglevel=INFO --time-limit=0 --concurrency=1
