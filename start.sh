7e72c2c49faff561b09a4bf930198f61faa240a5
nkhan364 <naveedkhan13041998@gmail.com>
2024-07-08 16:45:18 -0400

    major update

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-
 # Set the maximum memory limit to 300 MB
 ulimit -v $((300 * 1024))
 
 echo "STARTING HTTP SERVER..."
-python3 http_server.py 
+python3 http_server.py &  # Run HTTP server in background
 
 echo "STARTING CELERY WORKER WITH MEMORY LIMIT..."
 celery -A main worker --loglevel=INFO --time-limit=0 --concurrency=1
