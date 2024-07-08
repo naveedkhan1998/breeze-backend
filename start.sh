d03d383afceeb2e8a5f512a299e90fe85a9c657e
nkhan364 <naveedkhan13041998@gmail.com>
2024-07-08 14:38:00 -0400

    fix

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+python3 manage.py collectstatic --noinput
 python3 manage.py wait_for_db
 python3 manage.py makemigrations
-python3 manage.py migrate
+python3 manage.py migrate --noinput
 python3 manage.py initadmin
 rm /var/run/celery/w1.pid
 rm /var/run/celery/w2.pid
@@ -12,4 +13,4 @@ celery multi restart w1 w2 w3 -A main --pidfile=/var/run/celery/%n.pid --logfile
 #python3 manage.py runserver 0.0.0.0:5000
 #gunicorn main.wsgi:application --bind 0.0.0.0:5000 -w 1
 echo "STARTING GUNICORN SERVER..."
-gunicorn config.wsgi:application -t 1800 --bind :5000
+gunicorn main.wsgi:application -t 1800 --bind :5000
