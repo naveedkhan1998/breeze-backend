39eb3cc4c9bb5813251adc519236334ac59dbd45
nkhan364 <naveedkhan13041998@gmail.com>
2024-07-12 13:00:12 -0400

    final

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # Run collectstatic and wait_for_db in the background
-python3 manage.py collectstatic --noinput &
+# python3 manage.py collectstatic --noinput &
 python3 manage.py wait_for_db &
 
 # Wait for background jobs to finish
 wait
 
 # Run migrations and create an admin user
-python3 manage.py makemigrations
+#python3 manage.py makemigrations
 python3 manage.py migrate --noinput --skip-checks
 python3 manage.py initadmin
 # rm /var/run/celery/w1.pid
